99d689393df8c514eee631a6ff89daaa9d9463a0
Catalina Barnao <catalina.herrera02@alumnos.ucn.cl>
2024-05-09 19:55:59 -0400

    non-functional register sorter

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sorter;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class AuthController extends Controller
+{
+    public function sorter(Request $request)
+    {
+        dd($request->all());
+        $messages = [
+            'name.required' => 'Debe ingresar el campo nombre del sorteador',
+            'age.required' => 'Debe ingresar la edad del sorteador',
+            'age.integer' => 'La edad del sorteador debe ser numérica',
+            'age.between' => 'La edad del sorteador no puede ser inferior a 18 y mayor a 65',
+            'mail.required' => 'Debe ingresar el correo electrónico del sorteador',
+            'mail.unique' => 'El correo electrónico ingresado ya existe en el sistema',
+        ];
+
+        $validated= $request->validate([
+            'name'=>['required'],
+            'age' =>['required', 'integer', 'between:18,65'],
+            'mail'=>['required', 'unique:Sorters']
+        ],$messages);
+
+        do{
+            $password = Str::randomNumber(6, true);
+        }while(
+            $password[0] === '0'
+        );
+
+        Sorter::create([
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+            'password' => $password,
+            'status' => 1,
+        ]);
+
+        Mail::raw("Su contraseña es: $password", function ($message) use ($request){
+            $message->to($request->mail)->subject('Contraseña Lucky Go');
+        });
+
+        auth()->attempt([
+            'mail' => $request->mail,
+            'password' => $password,
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('auth.login');
@@ -8,3 +9,7 @@ Route::get('/', function () {
 Route::get('/register', function () {
     return view('auth.register');
 });
+Route::get('/sorter', function () {
+    return view('auth.sorter');
+});
+Route::post('sorter',[AuthController::class,'sorter'])->name('sorter');
