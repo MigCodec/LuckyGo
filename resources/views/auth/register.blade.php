faef049ee8d5729bbf5cf5a91dfb03fdded04c4b
Catalina Barnao <catalina.herrera02@alumnos.ucn.cl>
2024-05-10 00:31:56 -0400

    fixed logo on register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,8 +25,8 @@
 <body>
 
     <div class="container">
-        <img src="https://lh3.googleusercontent.com/pw/AP1GczMe3fo-AA66butYjilgqMojeACEilQxXtxfIOrmmFGlMUoU1BoTLm3-MV-5olvqcXUaUOTY9RhDSiErrVp6Jgd3Fw8gf-EZHuruO6Zp2KoeWGkkSosmXKDrrRjtoG_UPcofqNNvotCn1XPH5E2KRb5mxMGcSumpCPTrsU0M6Avv0my7OHLHwCXcYTSftU2Ye5pXRUcNynf05JbadZ8v0NPNunI9nZ6IPGUsPv1X8mhyWVwoLGMIj5i2FsQ73jdQ63xkWkZMA8SVX9qQgdiF8rZ-Pfquf65TZk4NRIv_kvoi_Q1CCKSwI6glK1K5vjSlIteSfBgT7wzC7xyzu5OCQxe2-rPfNXacyouj8Bfn0qkxxr3ozHnMK6cDODLSs0IvzR9OFjxjj106A3ykEvl_kEFAal1pURzsllHiBYcHF9a1vNYy9sUsWqFLOmc9K_pMGfTzmXA9PZbBMHS7uxdfB4FWAKFwf_xKvdKhur-bJ_FFIuqCXQ7rXlQtKz66z4aUPg45KN8Eapu1xOo_H9n3G0zBbakj8MM3cdIgXx0dMmH0VhmOaR7woCU6oxwBcSnu_BsXDswE10tgosK65OlZcE3mriqUFb2EIK55lGPPihOiw-99tX700DmuNPAMYRha-Zcw6_88mfNtREMux-6wXulnrr9xw3l5oobXopLQdT1VTkkTsg-fqxqEeBQYy1JgAHmuvd2vOQD_Jqf4ieZelEoRUp8GI5tch6blMlWlgABbnGrjS2nT2pcUzOLzt4LgtiCuSYEla3PnJwEaStPsyqj_VBVmz3KdXE-0wRVnzmG8uGfda2zLk5q2VZxN6oujpc6LujPCr21yRR8Q45VjYeKpJtPdy0u_CVsu-GGEnSS3V2vIGniQPwXHdD1bck7a3Ge4NsW8iS-wB3LknMEXV5lBwYiBGRK_gAhKUIIsgP1rvbkOObawOfCRAyvypxnU-8QmSqvsVJ_CA2nm9HStY3_qhFEAEpwTDofMoauH33QfDuUR4K8Cv1RtBHCWPOB2jzMD_r9U1usR3zOOEqxdJ6abimi_R9Nyb1Tr7Z2MFVn5ZdgB-Xr11Em0f6WQTiCSMV1sCBkMVA=w879-h879-s-no-gm?authuser=1" alt="Lucky Go Logo" 
-        style = "width: 160px; height: auto;">
+        <img src="luckygo_logo.png" alt="Lucky Go Logo" 
+        style = "width: 100px; height: auto;">
         
     </div>
     
