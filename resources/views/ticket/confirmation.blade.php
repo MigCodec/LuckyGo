37136f63e76a635daa19c47dfdbafbe6ec33dfb8
Catalina Barnao <catalina.herrera02@alumnos.ucn.cl>
2024-05-20 16:59:50 -0400

    SOR-001 | frontend | updated the pages to look like the examples on the requirement card

--- resources/views/ticket/confirmation.blade.php
+++ resources/views/ticket/confirmation.blade.php
@@ -3,18 +3,20 @@
 @section('title', 'Confirmación de Compra')
 
 @section('content')
-<div class="container">
-    <h1>Has seleccionado los numeros:</h1>
-    <p>{{ implode(', ', json_decode($numbers)) }}</p>
-    <p>El valor total de tu billete es ${{ number_format($price, 0, ',', '.') }}.</p>
-    <p>Deseas continuar?</p>
-    <form method="POST" action="{{ route('tickets.store') }}">
-        @csrf
-        <input type="hidden" name="numbers" value="{{ json_encode($numbers) }}">
-        <input type="hidden" name="im_feeling_lucky" value="{{ $im_feeling_lucky }}">
-        <input type="hidden" name="price" value="{{ $price }}">
-        <button type="submit" >Continuar</button>
-        <a href="{{ route('tickets.index') }}">Cancelar</a>
-    </form>
+<div style="text-align: center; font-family: Arial, sans-serif; ">
+    <div style="display: inline-block; margin: 0 auto; background-color: white; padding: 20px; border-radius: 8px; box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);">
+        <p>Has seleccionado los números:</p>
+        <p>{{ implode(' - ', json_decode($numbers)) }}</p>
+        <p>El valor total de tu billete es ${{ number_format($price, 0, ',', '.') }}.</p>
+        <b>¿Deseas continuar?</b>
+        <form method="POST" action="{{ route('tickets.store') }}">
+            @csrf
+            <input type="hidden" name="numbers" value="{{ json_encode($numbers) }}">
+            <input type="hidden" name="im_feeling_lucky" value="{{ $im_feeling_lucky }}">
+            <input type="hidden" name="price" value="{{ $price }}">
+            <button style="margin: 15px; background-color: #328000; color: white; padding: 10px 20px; border: none; border-radius: 8px; font-size: 16px; cursor: pointer;" type="submit" >Continuar</button>
+            <a href="{{ route('tickets.index') }}" style="margin: 15px; background-color: #EC2C00; color: white; padding: 10px 20px; border: none; border-radius: 8px; font-size: 16px; text-decoration: none;">Cancelar</a>
+        </form>
+    </div>
 </div>
 @endsection
