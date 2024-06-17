dac61f5d7119d3a31f7d5c11df684e4c9be649c8
Catalina Barnao <catalina.herrera02@alumnos.ucn.cl>
2024-06-17 01:52:01 -0400

    SOR-002 | frontend | updated the pages to look like the examples on the requeriment card

--- resources/views/lottery/index.blade.php
+++ resources/views/lottery/index.blade.php
@@ -3,67 +3,93 @@
 
 <!--
 This view displays a list of lotteries in a table format.-->
-<h1>
-    Listado de Sorteos
-</h1>
-<table>
-    <tr>
-        <th>
-            Fecha del sorteo
-        </th>
-        <th>
-            Cantidad de Billetes
-        </th>
-        <th>
-            Subtotal de Billetes
-        </th>
-        <th>
-            "Tendre Suerte"
-        </th>
-        <th>
-            Total
-        </th>
-        <th>
-            Estado
-        </th>
-        <th>
-            Ingresado por
-        </th>
-    </tr>
-    @foreach($lotteries as $lottery)
-    <tr>
-    <td>
-        {{$lottery->date}}
-    </td>
-    <td>
-        {{$lottery->count_total_tickets}}
-    </td>
-    <td>
-        {{$lottery->sum_price_normal_tickets}}
-    </td>
-    <td>
-        {{$lottery->sum_price_lucky_tickets}}
-    </td>
-    <td>
-        {{$lottery->sum_total_tickets   }}
-    </td>
-    <td>
-        @if($lottery->status==0)
-        Abierto
-        @endif
-        @if($lottery->status==1)
-        No Realizado
-        <form method="GET" action="{{ route('lotteries.register',$lottery->id) }}">
-            <button type="submit">Ingresar</button>
-        </form>
-        @endif
-        @if($lottery->status==2)
-        Realizado
-        @endif
-    </td>
-    <td>
-        {{$lottery->register_by}}
-    </td>
-    </tr>
-    @endforeach
+<div style="text-align: center;">
+    <h1>Listado de Sorteos</h1>
+    <div style="justify-content: center; align-items: center; display: flex;">
+        <table>
+            <tr>
+                <th>
+                    Fecha del sorteo
+                </th>
+                <th>
+                    Cantidad de Billetes
+                </th>
+                <th>
+                    Subtotal de Billetes
+                </th>
+                <th>
+                    "Tendré Suerte"
+                </th>
+                <th>
+                    Total
+                </th>
+                <th>
+                    Estado
+                </th>
+                <th>
+                    Ingresado por
+                </th>
+            </tr>
+            @foreach($lotteries as $lottery)
+            <tr>
+            <td>
+               {{$lottery->date}}
+            </td>
+            <td>
+                {{number_format($lottery->count_total_tickets,0,',','.')}}
+            </td>
+            <td>
+                ${{ number_format($lottery->sum_price_normal_tickets,0,',','.')}}
+            </td>
+            <td>
+                ${{number_format($lottery->sum_price_lucky_tickets,0,',','.')}}
+            </td>
+            <td>
+                ${{number_format($lottery->sum_total_tickets,0,',','.')   }}
+            </td>
+            <td>
+                @if($lottery->status==0)
+                Abierto
+                @endif
+                @if($lottery->status==1)
+                <div style="display:flex; text-align:center; align-items:center;">
+                    No realizado
+                    <form method="GET" action="{{ route('lotteries.register',$lottery->id) }}">
+                        <button type="submit" style="margin: 15px; background-color: #59D45C; padding: 10px 20px; border: none; border-radius: 8px; font-size: 16px; cursor: pointer;">Ingresar</button>
+                    </form>
+                </div>
+                @endif
+                @if($lottery->status==2)
+                Realizado
+                @endif
+            </td>
+            <td>
+                {{$lottery->register_by}}
+            </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
+<style>
+        table {
+            border-collapse: collapse; margin-top: 20px;
+        }
+
+        th, td {
+            border: 1px solid #ddd; padding: 12px; text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        tr:hover {
+            background-color: #f5f5f5;
+        }
+
+        table{
+            table-layout: auto;
+        }
+</style>
 @endsection
--- resources/views/lottery/store.blade.php
+++ resources/views/lottery/store.blade.php
@@ -4,90 +4,136 @@
 
 @section('content')
 <!--This view allows users to select their numbers to buy lottery tickets.-->
-<div class="container">
-    <h1>Selecciona tus Números</h1>
-    <table>
-        <tr>
-            <th>
-                Fecha del sorteo
-            </th>
-            <th>
-                Cantidad de Billetes
-            </th>
-            <th>
-                Subtotal de Billetes
-            </th>
-            <th>
-                "Tendre Suerte"
-            </th>
-            <th>
-                Total
-            </th>
-        </tr>
-        <tr>
-        <td>
-            {{$lottery->date}}
-        </td>
-        <td>
-            {{$lottery->count_total_tickets}}
-        </td>
-        <td>
-            {{$lottery->sum_price_normal_tickets}}
-        </td>
-        <td>
-            {{$lottery->sum_price_lucky_tickets}}
-        </td>
-        <td>
-            {{$lottery->sum_total_tickets   }}
-        </td>
-        <td>
+<div style="text-align: center;">
+    <h1>Registrar Sorteo</h1>
+    <div style="justify-content: center; align-items: center; display: flex;">
+        <table class="lottery_table">
+            <tr>
+                <th>
+                    Fecha del sorteo
+                </th>
+                <th>
+                    Cantidad de Billetes
+                </th>
+                <th>
+                    Subtotal de Billetes
+                </th>
+                <th>
+                    "Tendre Suerte"
+                </th>
+                <th>
+                    Total
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    {{$lottery->date}}
+                </td>
+                <td>
+                    {{number_format($lottery->count_total_tickets,0,',','.')}}
+                </td>
+                <td>
+                    ${{ number_format($lottery->sum_price_normal_tickets,0,',','.')}}
+                </td>
+                <td>
+                    ${{number_format($lottery->sum_price_lucky_tickets,0,',','.')}}
+                </td>
+                <td>
+                    ${{number_format($lottery->sum_total_tickets,0,',','.')   }}
+                </td>
+            </tr>
         </table>
-    <form method="POST" action="{{ route('lotteries.store') }}">
-        @csrf
-        <input name="lottery_id" type="hidden" value="{{$lottery->id}}"/>
-          <!-- Select lucky numbers -->
-        <div class="form-group">
-            <label for="numbers">Sorteo</label>
-            <table>
-                @for ($i = 0; $i < 5; $i++)
-                    <tr>
-                        @for ($j = 1; $j <= 6; $j++)
-                            @php
-                                $number = $i * 6 + $j;
-                            @endphp
-                            <td>
-                                <input type="checkbox" id="number_{{ $number }}" name="normal_numbers[]" value="{{ $number }}">
-                                <label for="number_{{ $number }}">{{ $number }}</label>
-                            </td>
-                        @endfor
-                    </tr>
-                @endfor
-            </table>
-        </div>
-        <div class="form-group">
-            <label for="lucky_numbers">Tendre Suerte</label>
-            <table>
-                @for ($i = 0; $i < 5; $i++)
-                    <tr>
-                        @for ($j = 1; $j <= 6; $j++)
-                            @php
-                                $number = $i * 6 + $j;
-                            @endphp
-                            <td>
-                                <input type="checkbox" id="lucky_number_{{ $number }}" name="lucky_numbers[]" value="{{ $number }}">
-                                <label for="lucky_number_{{ $number }}">{{ $number }}</label>
-                            </td>
-                        @endfor
-                    </tr>
-                @endfor
-            </table>
-        </div>
-         <!-- Submit and cancel buttons -->
-        <button type="submit">Confirmar</button>
-        <a href="{{ route('lotteries.index') }}" style="margin: 15px; background-color: #EC2C00; color: white; padding: 10px 20px; border: none; border-radius: 8px; font-size: 16px; text-decoration: none;">Cancelar</a>
-        @error('numbers')
-        {{$message}}
-        @enderror
-    </form>
+    </div>
+
+    <div style="display: flex; justify-content: center; margin-top: 20px;">
+        <form method="POST" action="{{ route('lotteries.store') }}" style="text-align: center;">
+            @csrf
+            <input name="lottery_id" type="hidden" value="{{$lottery->id}}"/>
+            <!-- Select lucky numbers -->
+            <div style="display: flex; justify-content: space-between; margin-bottom: 10px;">
+                <div style="width: 45%;">
+                    <div class="form-group">
+                        <label for="numbers"><b>Sorteo</b></label>
+                        <table style="margin-top: 10px;">
+                            @for ($i = 0; $i < 5; $i++)
+                                <tr>
+                                    @for ($j = 1; $j <= 6; $j++)
+                                        @php
+                                            $number = $i * 6 + $j;
+                                        @endphp
+                                        <td style="padding: 5px;">
+                                            <input type="checkbox" id="number_{{ $number }}" name="normal_numbers[]" value="{{ $number }}" style="display: none">
+                                            <label for="number_{{ $number }}" style="display: inline-block; width: 65px; height: 40px; line-height: 40px; text-align: center; background-color: #F2F2F2; cursor: pointer;">{{ $number }}</label>
+                                            <style>
+                                                #number_{{$number}}:checked + label {
+                                                    background-color: #F4B700 !important;
+                                                }
+                                            </style>
+                                        </td>
+                                    @endfor
+                                </tr>
+                            @endfor
+                        </table>
+                    </div>
+                </div>
+
+                <div style="width: 1px; background-color: black; margin: 20px; margin-left: 40px; margin-top: 35px; margin-bottom: 5px;"></div>
+            
+                <div style="width: 45%">
+                    <div class="form-group">
+                        <label for="lucky_numbers"><b>Tendré Suerte</b></label>
+                        <table style="margin-top: 10px;">
+                            @for ($i = 0; $i < 5; $i++)
+                                <tr>
+                                    @for ($j = 1; $j <= 6; $j++)
+                                        @php
+                                            $number = $i * 6 + $j;
+                                        @endphp
+                                        <td style="padding: 5px;">
+                                            <input type="checkbox" id="lucky_number_{{ $number }}" name="lucky_numbers[]" value="{{ $number }}" style="display: none">
+                                            <label for="lucky_number_{{ $number }}" style="display: inline-block; width: 65px; height: 40px; line-height: 40px; text-align: center; background-color: #F2F2F2; cursor: pointer;">{{ $number }}</label>
+                                            <style>
+                                                #lucky_number_{{$number}}:checked + label {
+                                                    background-color: #F4B700 !important;
+                                                }
+                                            </style>
+                                        </td>
+                                    @endfor
+                                </tr>
+                            @endfor
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- Submit and cancel buttons -->
+            <button type="submit" style="margin: 15px; background-color: #317C00; color: white; padding: 10px 20px; border: none; border-radius: 8px; font-size: 16px; text-decoration: none; cursor: pointer;">Confirmar</button>
+            <a href="{{ route('lotteries.index') }}" style="margin: 15px; background-color: #EC2C00; color: white; padding: 10px 20px; border: none; border-radius: 8px; font-size: 16px; text-decoration: none;">Cancelar</a>
+            @error('numbers')
+            {{$message}}
+            @enderror
+        </form>
+    </div>
 </div>
+
+<style>
+        .lottery_table {
+            border-collapse: collapse; margin-top: 20px;
+        }
+
+        .lottery_table th, .lottery_table td {
+            border: 1px solid #ddd; padding: 12px; text-align: left;
+        }
+
+        .lottery_table th {
+            background-color: #f2f2f2;
+        }
+
+        .lottery_table tr:hover {
+            background-color: #f5f5f5;
+        }
+
+        .lottery_table table{
+            table-layout: auto;
+        }
+</style>
 @endsection
